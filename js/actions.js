dcdd0d834d63488e7bb0b9c2b83fff87aaff1d9a
Ryan <rheupler@gmail.com>
2016-12-23 23:07:28 -0500

    adds functionality for returning subreddit posts and authors

--- js/actions.js
+++ js/actions.js
@@ -1,12 +1,33 @@
-function receivePosts(subreddit, json) {
-  return {
-    type: RECEIVE_POSTS,
-    subreddit.,
-    posts: json.data.children.map(child => child.data),
-    received_at: Date.now()
-  }
+const pusher = new Pusher("50ed18dd967b455393ed");
+
+
+function getValue() {
+  let subredditChannel = document.getElementById("input-box").value;
+  alert(subredditChannel);
+  subredditChannel.bind("new-listing", function(listing) {
+      console.log(`${listing.author} wants to know: ${listing.title}`);
+  });
 }
 
-function fetchPosts(subreddit) {
-    return fetch(`https://reddit.com/${subreddit}.json`);
-}
+//
+
+// function outputValue() {
+//   document.getElementById('sub-form').submit(function() {
+//     console.log(userVal);
+//   })
+// }
+
+
+
+// function receivePosts(subreddit, json) {
+//   return {
+//     type: RECEIVE_POSTS,
+//     subreddit,
+//     posts: json.data.children.map(child => child.data),
+//     received_at: Date.now()
+//   }
+// }
+//
+// function fetchPosts(subreddit) {
+//     return fetch(`https://reddit.com/${subreddit}.json`);
+// }
--- js/pusher.js
+++ js/pusher.js
@@ -1,9 +1,12 @@
-const pusher = new Pusher("50ed18dd967b455393ed");
-
-const subredditChannel = pusher.subscribe("askreddit");
-
-subredditChannel.bind("new-listing", function(listing) {
-    console.log(listing);
-  });
-
-// console.log(subredditChannel)
+// const pusher = new Pusher("50ed18dd967b455393ed");
+//
+// const subredditChannel = pusher.subscribe("askreddit");
+//
+// subredditChannel.bind("new-listing", function(listing) {
+//     // let p = document.createElement('p');
+//     // p.innerHTML = `${listing.author}`;
+//     // document.getElementsByClassName('new-post').appendChild(p);
+//     // // console.log(`${listing.author} wants to know: ${listing.title}`);
+//   });
+//
+// // console.log(subredditChannel)
